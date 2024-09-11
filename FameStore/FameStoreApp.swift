eb3868b0348356381ab442b87c3a7b5297a41503
MetehanCanpolat <metehancanpolat@outlook.com>
2024-09-11 09:34:01 +0300

    Initial Commit

---
+++ FameStore/FameStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FameStoreApp.swift
+//  FameStore
+//
+//  Created by Metehan Canpolat on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FameStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
